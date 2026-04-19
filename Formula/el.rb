bf9f5900e06038ff4b7b0bef1d33d26425505b9e
msuarz <msuarz@limadelic.com>
2026-04-19 17:56:55 -0400

    bump to v0.1.14

--- Formula/el.rb
+++ Formula/el.rb
@@ -4,11 +4,19 @@ class El < Formula
   license "MIT"
   version "0.1.14"
 
-  url "https://github.com/limadelic/el/releases/download/v0.1.14/el_macos_arm64"
-  sha256 "fd1e81a9e2c58ce2e15a2ae7e3e3088f3beb3ad745fe2f229df96ef33c3ab6eb"
+  on_macos do
+    on_arm do
+      url "https://github.com/limadelic/el/releases/download/v0.1.14/el_macos_arm64"
+      sha256 "e81b56901386fadceef5dd82756c01b8b6ffac82a933f5f4f2377b67a3cf5313"
+    end
+    on_intel do
+      url "https://github.com/limadelic/el/releases/download/v0.1.14/el_macos_x86_64"
+      sha256 "529597bf68bfedc5d7de01423c4fbd153fa863b06321e6c8b7ab42b84017a43e"
+    end
+  end
 
   def install
-    bin.install "el_macos_arm64" => "el"
+    bin.install "el_macos_#{arch}" => "el"
   end
 
   test do
