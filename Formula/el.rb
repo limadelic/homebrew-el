e5341e6cea6fb4fcfb6a0b2160fef1a758b37d56
msuarz <maykel.suarez@ukg.com>
2026-04-20 21:35:46 -0400

    bump to v0.1.17

--- Formula/el.rb
+++ Formula/el.rb
@@ -5,7 +5,7 @@ class El < Formula
   version "0.1.17"
 
   url "https://github.com/limadelic/el/releases/download/v0.1.17/el_macos_arm64"
-  sha256 "78c7aad6f0befe6edbad2bf656c92c476240b2cd8ab338a24aac6f15828d9e84"
+  sha256 "4476fa675e9728f858ec5236c00dee0db2ad2176360bfab28b3f75d3aa0b32f7"
 
   def install
     bin.install "el_macos_arm64" => "el"
