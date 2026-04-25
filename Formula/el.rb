1b63f06cf95e99100e069f135ee49f4bbc15e5e2
msuarz <maykel.suarez@ukg.com>
2026-04-25 17:52:06 -0400

    el 0.1.82

--- Formula/el.rb
+++ Formula/el.rb
@@ -2,14 +2,18 @@ class El < Formula
   desc "CLI for managing headless Claude Code sessions"
   homepage "https://github.com/limadelic/el"
   license "MIT"
-  version "0.1.81"
+  version "0.1.82"
 
-  url "https://github.com/limadelic/el/releases/download/v0.1.81/el-0.1.81.tar.gz"
-  sha256 "f41a27f6d330a912b5067609a0fb8ee19cce35c8b23545c198abdc8a162d190c"
+  url "https://github.com/limadelic/el/releases/download/v0.1.82/el-0.1.82.tar.gz"
+  sha256 "4c1acf8c1ab70513f8ab8992ccae48d0e308c1d14496ff9071c1e7a4ad3bea85"
 
   def install
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/el_wrapper" => "el"
+  end
+
+  def post_install
+    quiet_system libexec/"bin/el", "stop"
   end
 
   test do
